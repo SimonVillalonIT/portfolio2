bdafbbfd7b51c92f8806209a3eca166bb17aeeab
SimonVillalonIT <simonvillalon10@gmail.com>
2023-06-16 23:12:51 -0300

    Advances in the hero section

---
+++ src/data/Data.ts
@@ -0,0 +1,34 @@
+export const header = {
+  title: "Portafolio.",
+  links: [
+    {
+      title: "Inicio",
+      href: "#home",
+    },
+    {
+      title: "Habilidades",
+      href: "#home",
+    },
+    {
+      title: "Portafolio",
+      href: "#portfolio",
+    },
+    {
+      title: "Contacto",
+      href: "#contact",
+    },
+  ],
+};
+
+export const hero = {
+  gretting: "Hola, encantado soy",
+  name: "Simón Villalón",
+  role: "Desarrollador Fullstack",
+  paragraph:
+    "Soy un desarrollador web entusiasmado en crear soluciones digitales innovadoras y colaborar en proyectos emocionantes.",
+  socialNetworks: [
+    { name: "mdi:linkedin", link: "" },
+    { name: "mdi:github", link: "" },
+  ],
+  button: "Descargar CV",
+};
